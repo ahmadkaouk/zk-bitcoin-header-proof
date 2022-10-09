b85b63aaf8a3812bfb5999b595cfe5a35564ffc0
Ahmad <ahmad@MacBook.local>
2022-10-09 23:13:37 +0200

    Add BlockHeader methods

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,6 @@
+use primitive_types::U256;
 use serde::{Deserialize, Serialize};
-
+use sha2::{Sha256, Digest};
 /// A Bitcoin block header, which contains all the block information
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct BlockHeader {
@@ -12,32 +13,50 @@ pub struct BlockHeader {
 }
 
 impl BlockHeader {
-    /// Get Little Endian bytes representation of the block
-    pub fn to_le_bytes(&self) -> [u8; 80] {
-        [
-            self.version.to_le_bytes().into(),
-            self.prev_blockhash.into_iter().rev().collect::<Vec<_>>(),
-            self.merkle_root.into_iter().rev().collect::<Vec<_>>(),
-            self.time.to_le_bytes().into(),
-            self.bits.to_le_bytes().into(),
-            self.nonce.to_le_bytes().into(),
-        ]
-        .into_iter()
-        .flatten()
-        .collect::<Vec<_>>()
-        .try_into()
-        .expect("Cannot extract bytes from Block Header")
+    /// Compute the hash of the block header.
+    pub fn block_hash(&self) -> Vec<u8> {
+        // Step 1 Transform block into little endian hexadecimal (in form of bytes)
+        let block_le = bincode::serialize(&self).unwrap();
+
+        // Step 2 Apply a SHA-256 on the binary representation of the litte endian hex obtained in step 1
+        let mut hasher = Sha256::new();
+        hasher.update(block_le);
+        let hash1 = hasher.finalize();
+
+        // Step 3 Apply a SHA-256 on the hash obtained in step 2
+        let mut hasher = Sha256::new();
+        hasher.update(hash1);
+        let mut hash = hasher.finalize().to_vec();
+       
+        // Step 4 Convert the result into little endian 
+        hash.reverse();
+        hash
     }
 
     /// Compute target from bits
     /// formula : target = coefficient * 2 ^ (8 * (index — 3))
-    pub fn target(&self) -> [u64; 4] {
-        // TODO Compute target from bits
-        [0; 4]
+    pub fn target(&self) -> U256 {
+        let (target, expt) = {
+            let unshifted_expt = self.bits >> 24;
+            if unshifted_expt <= 3 {
+                (
+                    (self.bits & 0xFFFFFF) >> (8 * (3 - unshifted_expt as usize)),
+                    0,
+                )
+            } else {
+                (self.bits & 0xFFFFFF, 8 * ((self.bits >> 24) - 3))
+            }
+        };
+
+        if target > 0x7FFFFF {
+            Default::default()
+        } else {
+            U256::try_from(target as u64).unwrap() << (expt as usize)
+        }
     }
 }
 
-/// Contains Block Header Informations to be commited to the journal
+/// Contains Block Header informations to be commited to the journal
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct BlockHeaderCommit {
     pub version: i32,
@@ -45,4 +64,59 @@ pub struct BlockHeaderCommit {
     pub merkle_root: [u8; 32],
     pub time: u32,
     pub bits: u32,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex::FromHex;
+
+    #[test]
+    fn test_valid_block_hex() {
+        let block_hex = "010000009500c43a25c624520b5100adf82cb9f9da72fd2447a496bc600b0000000000006cd862370395dedf1da2841ccda0fc489e3039de5f1ccddef0e834991a65600ea6c8cb4db3936a1ae3143991";
+
+        // Parse the hex data into a block header value
+        let b: BlockHeader = bincode::deserialize(&hex::decode(block_hex).unwrap()).unwrap();
+
+        assert_eq!(
+            b,
+            BlockHeader {
+                version: 1,
+                prev_blockhash: Vec::from_hex(
+                    "9500c43a25c624520b5100adf82cb9f9da72fd2447a496bc600b000000000000"
+                )
+                .unwrap()
+                .try_into()
+                .unwrap(),
+                merkle_root: Vec::from_hex(
+                    "6cd862370395dedf1da2841ccda0fc489e3039de5f1ccddef0e834991a65600e"
+                )
+                .unwrap()
+                .try_into()
+                .unwrap(),
+                time: 1305200806,
+                bits: 443192243,
+                nonce: 2436437219
+            }
+        )
+    }
+
+    #[test]
+    fn test_block_hash() {
+        let block_hex = "010000009500c43a25c624520b5100adf82cb9f9da72fd2447a496bc600b0000000000006cd862370395dedf1da2841ccda0fc489e3039de5f1ccddef0e834991a65600ea6c8cb4db3936a1ae3143991";
+        // Parse the hex data into a block header value
+        let block: BlockHeader = bincode::deserialize(&hex::decode(block_hex).unwrap()).unwrap();
+        assert_eq!(
+            block.block_hash(),
+            hex::decode("0000000000002917ed80650c6174aac8dfc46f5fe36480aaef682ff6cd83c3ca")
+                .unwrap()
+        );
+    }
+    #[test]
+    fn test_block_target_from_bits() {
+        let block_hex = "010000009500c43a25c624520b5100adf82cb9f9da72fd2447a496bc600b0000000000006cd862370395dedf1da2841ccda0fc489e3039de5f1ccddef0e834991a65600ea6c8cb4db3936a1ae3143991";
+        let block: BlockHeader = bincode::deserialize(&hex::decode(block_hex).unwrap()).unwrap();
+
+        assert_eq!(block.target(), U256::from_str_radix("0x6a93b30000000000000000000000000000000000000000000000", 16).unwrap());
+    }
 }
--- methods/guest/src/bin/validator.rs
+++ methods/guest/src/bin/validator.rs
@@ -1,21 +1,27 @@
 #![no_main]
 #![no_std]
 
+use block_header_core::*;
 use risc0_zkvm_guest::{env, sha};
-use block_header_zkproof_core::*;
 
 risc0_zkvm_guest::entry!(main);
 
 pub fn main() {
     // Load the block to validate from the host
-    let block : BlockHeader = env::read();
+    let block: BlockHeader = env::read();
 
-    // let block_hash = sha::
+    // Compute the hash of the block header
+    // let block_hash = sha::raw_digest(sha::digest_u8_slice(&block.block_hash()));
+    let target = block.target();
+
     // Commit Block Header constituents to the header
-    env::commit({&BlockHeaderCommit {
-        version: block.version,
-        prev_blockhash: block.prev_blockhash,
-        merkle_root: block.merkle_root,
-        bits: block.bits
-    }});
+    env::commit({
+        &BlockHeaderCommit {
+            version: block.version,
+            prev_blockhash: block.prev_blockhash,
+            merkle_root: block.merkle_root,
+            time: block.time,
+            bits: block.bits,
+        }
+    });
 }
